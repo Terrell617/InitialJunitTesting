b086573e7cbecf730125d69ca429c34ff6e35254
Terrell Mallory <Terrell.Mallory@gmail.com>
2021-12-22 22:05:11 -0500

    JunitTesting1

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -7,17 +7,25 @@ class CalculatorTest {
 
     @Test
     @DisplayName("Add two numbers")
-    void add() {
+    public void add() {
         assertEquals(4, Calculator.add(2, 2));
     }
 
     @Test
     @DisplayName("Multiply two numbers")
-    void multiply() {
+    public void multiply() {
         assertAll(() ->
                         assertEquals(4, Calculator.multiply(2, 2)),
                 () -> assertEquals(-4, Calculator.multiply(2, -2)),
                 () -> assertEquals(4, Calculator.multiply(-2, -2)),
                 () -> assertEquals(0, Calculator.multiply(1, 0)));
+    }
+
+    @org.junit.jupiter.api.Test
+    void testAdd() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testMultiply() {
     }
 }
